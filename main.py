7c56f922b15563341951254bb56a67045e561e3e
Nick Paschke <Nickbot606@users.noreply.github.com>
2024-09-25 20:05:07 -0400

    Added further features
    - Added multiple select  
    - changed radial buttons to check boxes, added dynamic spacing as well as wrapping text.
    - Added keyboard listener (a - e as well as enter)
    - Added More than 4 options at a time with dynamic rendering for questions
    - Added resizability (non-reactive) to the window
    - Made the font smaller

--- main.py
+++ main.py
@@ -1,147 +1,136 @@
-# Python program to create a simple GUI
-# Simple Quiz using TKinter
-
-# import everything from tkinter
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox as mb
-
-#import json to use json file for data
 import json
 
-#class to define the components of the GUI
 class Quiz:
     def __init__(self):
-
-        # set question number to 0
         self.q_no = 0
-
-        #assigns ques to the display_question function to update later.
         self.display_title()
+        self.datasize = len(question)
+        self.correct = 0
+        
+        # Initialize the list of BooleanVar once
+        self.opt_selected = [tk.BooleanVar() for _ in range(5)]
+        self.opts = self.check_boxes()
+        
         self.display_question()
-        self.opt_selected = IntVar()
-        self.opts = self.radio_buttons()
         self.display_options()
         self.buttons()
-        self.data_size = len(question)
-        self.correct = 0
 
-    # This method is used to display the result.
-    # It counts the number of correct and wrong answers
-    # and display them at the end as a message box.
+        # Keyboard bindings
+        root.bind('<Return>', lambda event: self.next_btn())  # Enter key
+        root.bind('<a>', lambda event: self.select_option(0))
+        root.bind('<b>', lambda event: self.select_option(1))
+        root.bind('<c>', lambda event: self.select_option(2))
+        root.bind('<d>', lambda event: self.select_option(3))
+        root.bind('<e>', lambda event: self.select_option(4))
+
+    def clear_previous_question(self):
+        for i in range(5):
+            self.opt_selected[i].set(False)
+
     def display_result(self):
+        wrong_count = self.datasize - self.correct
+        score = int(self.correct / self.datasize * 100)
+        result = f"Score {score}%\nCorrect: {self.correct}\nWrong: {wrong_count}"
+        mb.showinfo("Result", result)
 
-        # calculates the wrong count
-        wrong_count = self.data_size - self.correct
-        correct = f"Correct: {self.correct}"
-        wrong = f"Wrong: {wrong_count}"
-
-        # calculates the percentage of correct answers
-        score = int(self.correct / self.data_size * 100)
-        result = f"Score: {score}%"
-
-        #shows a message box to display the result
-        mb.showinfo("Result", f"{result}\n{correct}\n{wrong}")
-
-    # This method checks the answer after we click on Next.
     def check_ans(self, q_no):
+        selected_answers = [var.get() for var in self.opt_selected]
+        correct_answers = answer[q_no]
 
-        # checks if the selected option is correct
-        if self.opt_selected.get() == answer[q_no]:
-            return True
+        selected_indices = [i + 1 for i, selected in enumerate(selected_answers) if selected]
 
-    # This method is used to check the answer of the
-    # current question by calling the check_ans and question no.
-    def next_btn(self):
-
-        # Check if the answer is correct
-        if self.check_ans(self.q_no):
-
-            # if the answer is correct it increments the correct by 1
-            self.correct += 1
-
-        # Moves to next question by incrementing the q_no counter
-        self.q_no += 1
-        if self.q_no == self.data_size:
-            self.display_result()
-
-            # destroys the GUI
-            gui.destroy()
+        if isinstance(correct_answers, list):
+            if sorted(selected_indices) == sorted(correct_answers):
+                print("RIGHT!")
+                return True
         else:
-            #shows the next question
+            if selected_indices == [correct_answers]:
+                print("RIGHT!")
+                return True
+        
+        print("WRONG!\n")
+        print("You answered: " + str(selected_indices))
+        print(question[q_no])
+
+        if isinstance(correct_answers, list):
+            for x in correct_answers:
+                print(options[q_no][x-1])
+        else:
+            print(options[q_no][correct_answers-1])
+        return False
+
+    def next_btn(self):
+        if self.check_ans(self.q_no):
+            self.correct += 1
+        self.q_no += 1
+        if self.q_no == self.datasize:
+            self.display_result()
+            root.destroy()
+        else:
             self.display_question()
+            self.clear_previous_question()
             self.display_options()
 
-    # This method shows the two buttons on the screen
     def buttons(self):
+        next_buttons = tk.Button(root, text="Next", command=self.next_btn, width=10, bg="black", fg="white", font=("arial", 16, "bold"))
+        next_buttons.place(x=350, y=380)
+        quit_buttons = tk.Button(root, text="Quit", command=root.destroy, width=5, bg="black", fg="white", font=("arial", 16, "bold"))
+        quit_buttons.place(x=700, y=50)
 
-        # The first button is the next button to move to the
-        # next question.
-        next_button = Button(gui, text="Next", command=self.next_btn,
-                             width=10, bg="blue", fg="white", font=("ariel", 16, "bold"))
-
-        # Placing button on screen
-        next_button.place(x=350, y=380)
-
-        #second button which is used to quit the GUI
-        quit_button = Button(gui, text="Quit", command=gui.destroy,
-                             width=5, bg="red", fg="white", font=("ariel", 16, " bold"))
-        quit_button.place(x=700, y=50)
-
-    # This method is used to deselect the radio buttons on the screen.
-    def display_options(self):
-        val = 0
-        self.opt_selected.set(0)
-        for option in options[self.q_no]:
-            self.opts[val]['text'] = option
-            val += 1
-
-    # This method shows the current question on the screen
     def display_question(self):
-        q_no = Label(gui, text=question[self.q_no], width=60,
-                     font=('ariel', 16, 'bold'), anchor='w')
+        for widget in root.winfo_children():
+            if isinstance(widget, tk.Label) and widget.winfo_y() >= 100:
+                widget.destroy()
+        
+        q_no = tk.Label(root, text=question[self.q_no], width=300, wraplength=600, font=("arial", 10, "bold"), anchor="w")
         q_no.place(x=70, y=100)
 
-    # This method is used to display title
     def display_title(self):
-        title = Label(gui, text="QUIZ MODULE",
-                      width=50, bg="orange", fg="black", font=("ariel", 20, "bold"))
+        title = tk.Label(text="MCQ Quiz", width=50, bg="green", fg="red", font=("arial", 20, "bold"))
         title.place(x=0, y=2)
 
-    # This method shows the radio buttons to select the question on the screen
-    #at the specified position.
-    def radio_buttons(self):
+    def display_options(self):
+        # Get the height of the question label
+        question_label = root.winfo_children()[-1]  # The last child should be the question label
+        question_height = question_label.winfo_height()
+        
+        base_y = 100 + question_height + 80  # Start below the question, with a bit of padding
+        for i in range(5):
+            if i < len(options[self.q_no]):
+                self.opts[i]["text"] = options[self.q_no][i]
+                self.opts[i].place(x=180, y=base_y + (i * 40))
+                self.opts[i].select() if self.opt_selected[i].get() else self.opts[i].deselect()
+                self.opts[i].config(state=tk.NORMAL)
+            else:
+                self.opts[i].place_forget()  # Hide option if not present
+
+    def check_boxes(self):
         q_list = []
-        y_pos = 150
-        while len(q_list) < 4:
-            radio_btn = Radiobutton(gui, text=" ", variable=self.opt_selected,
-                                    value=len(q_list) + 1, font=("ariel", 14))
+        for i in range(5):
+            radio_btn = tk.Checkbutton(root, text="", variable=self.opt_selected[i], font=("arial", 10))
             q_list.append(radio_btn)
-            radio_btn.place(x=100, y=y_pos)
-            y_pos += 40
         return q_list
 
-# Create a GUI Window
-gui = Tk()
+    def select_option(self, index):
+        if index < len(self.opts) and self.opts[index].cget("state") != "disabled":
+            self.opt_selected[index].set(not self.opt_selected[index].get())
+            self.opts[index].select() if self.opt_selected[index].get() else self.opts[index].deselect()
 
-# set the size of the GUI window
-gui.geometry("800x450")
-
-# set the title of the window
-gui.title("GET QUIZ")
-
-# get the data from the json file
-with open('data.json') as f:
+# Load data from JSON
+with open("practice-exam-15.json", "r") as f:
     data = json.load(f)
 
-# set the questions, options, and answer
-question = (data['question'])
-options = (data['options'])
-answer = (data['answer'])
+question = data["question"]
+answer = data["answer"]
+options = data["options"]
 
-# Create an object of the quiz class.
+# Start quiz
+root = tk.Tk()
+root.title("MCQ QUIZ")
+root.geometry("800x450")
+root.resizable(True, True)
+
 quiz = Quiz()
-
-# Start the GUI
-gui.mainloop()
-
-# END OF THE PROGRAM....
+root.mainloop()
